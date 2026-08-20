34f34aa5e10bf8695cb4ea292d9129a9e81bb778
ama-jay <amandimalshika@gmail.com>
2026-08-20 13:01:11 +0530

    Implement  backend Task 4 update move and delete API

--- backend/src/repositories/taskRepository.js
+++ backend/src/repositories/taskRepository.js
@@ -1,19 +1,49 @@
 import tasks from '../utils/mockTasks.js';
 
-let keyCounter = 102; 
+let keyCounter = 102;
 
 const taskRepository = {
   getAllTasks: () => tasks,
+
   getTaskById: (id) => tasks.find(task => task.id === id),
+
   createTask: (taskData) => {
     const prefix = taskData.type === 'BUG' ? 'BUG' : 'TASK';
-    const newTask = { 
-        id: `${prefix}-${keyCounter++}`, 
-        ...taskData,
-        status: taskData.status || 'To Do' 
+
+    const newTask = {
+      id: `${prefix}-${keyCounter++}`,
+      ...taskData,
+      status: taskData.status || 'To Do'
     };
+
     tasks.push(newTask);
     return newTask;
+  },
+
+  // Update
+  updateTask: (id, updates) => {
+    const task = tasks.find(task => task.id === id);
+
+    if (!task) {
+      return null;
+    }
+
+    Object.assign(task, updates);
+
+    return task;
+  },
+
+  // Delete
+  deleteTask: (id) => {
+    const index = tasks.findIndex(task => task.id === id);
+
+    if (index === -1) {
+      return null;
+    }
+
+    const deletedTask = tasks.splice(index, 1)[0];
+
+    return deletedTask;
   }
 };
 
